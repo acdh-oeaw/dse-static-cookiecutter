614973b8058f127ae38773d96b1628d6fcdcd03a
csae8092 <peter.andorfer@oeaw.ac.at>
2023-05-21 12:02:31 +0200

    wip #24

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,3 +5,9 @@ if "{{ cookiecutter.update_favicons }}" == "yes":
     print("you can run `python update_favicons.py")
 else:
     os.remove("update_favicons.py")
+
+if "{{ cookiecutter.search_engine }}" == "typesense":
+    print("remove static search dependencies")
+    os.remove("./shellscripts/dl_staticsearch.sh")
+    os.remove("./ss_config.xml")
+    os.remove("./shellscripts/build_index.sh")
--- {{cookiecutter.directory_name}}/shellscripts/script.sh
+++ {{cookiecutter.directory_name}}/shellscripts/script.sh
@@ -7,5 +7,5 @@ script_location_dir="${BASH_SOURCE[0]}"
 cd $script_location_dir && cd ..
 # run scripts
 ./shellscripts/dl_imprint.sh
-./shellscripts/dl_saxon.sh
-./shellscripts/dl_staticsearch.sh
+./shellscripts/dl_saxon.sh{% if cookiecutter.search_engine == 'staticsearch' %}
+./shellscripts/dl_staticsearch.sh{% endif %}
