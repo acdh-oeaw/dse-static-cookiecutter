d5e9f8aa80b8ff153bc711f6023b7587753a2b47
Your Name <c.f.haak@posteo.de>
2025-01-09 17:01:21 +0100

    Fixed some orthographic horrors definitely not caused by me ;) see #82

--- {{cookiecutter.directory_name}}/shellscripts/script.sh
+++ {{cookiecutter.directory_name}}/shellscripts/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# hacky way to make shure the script 
-# gets allways run from parent-dir 
-# so relative paths get resolved the righ way
+# hacky way to make sure the script 
+# gets always run from parent-dir 
+# so relative paths get resolved the right way
 script_location_dir="${BASH_SOURCE[0]}"
 cd $script_location_dir && cd ..
 # run scripts
