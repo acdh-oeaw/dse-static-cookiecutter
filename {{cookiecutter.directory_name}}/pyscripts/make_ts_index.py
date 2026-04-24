b0253402d9cba49f5f02f686ef611d27a77da552
csae8092 <p.andorfer@gmail.com>
2026-04-24 09:58:27 +0200

    fix: be aware of language label

--- {{cookiecutter.directory_name}}/pyscripts/make_ts_index.py
+++ {{cookiecutter.directory_name}}/pyscripts/make_ts_index.py
@@ -80,7 +80,7 @@ for x in tqdm(files, total=len(files)):
         item["id"] = get_xmlid(y)
         item["label"] = make_entity_label(
             y.xpath("./tei:persName[1]", namespaces=namespaces)[0]
-        )
+        )[0]
         record["person_entities"].append(item)
 
     record["place_entities"] = []
@@ -89,7 +89,7 @@ for x in tqdm(files, total=len(files)):
         item["id"] = get_xmlid(y)
         item["label"] = make_entity_label(
             y.xpath("./tei:placeName[1]", namespaces=namespaces)[0]
-        )
+        )[0]
         record["place_entities"].append(item)
 
     record["org_entities"] = []
@@ -98,7 +98,7 @@ for x in tqdm(files, total=len(files)):
         item["id"] = get_xmlid(y)
         item["label"] = make_entity_label(
             y.xpath("./tei:orgName[1]", namespaces=namespaces)[0]
-        )
+        )[0]
         record["org_entities"].append(item)
 
     record["bibl_entities"] = []
